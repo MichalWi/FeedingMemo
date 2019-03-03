8dd19a752ee39382e16beecd62aa8373ed48c345
Mich W <michalw@outlook.com>
2019-03-03 21:41:16 +0100

    nicer graphics

--- Feeding Memo/TableViewController.swift
+++ Feeding Memo/TableViewController.swift
@@ -18,7 +18,8 @@ struct tableViewConsts {
     static let buttonSize : CGFloat = 50
 }
 
-class TableViewController: UITableViewController, CircleMenuDelegate {
+class TableViewController: UITableViewController, UIGestureRecognizerDelegate, CircleMenuDelegate { 
+    
     
     
     let DB = FeedingSessionService()
@@ -26,30 +27,26 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
     let reminderService = ReminderService()
     
     @IBAction func ReminderSwitchDidChange(_ sender: Any) {
-        if RemindSwitch.isOn {
-            reminderService.addFeedingReminder(fromSession: feedData.first)
-        } else {
-            reminderService.removeReminder()
-        }
+//        if RemindSwitch.isOn {
+//            reminderService.addFeedingReminder(fromSession: feedData.first)
+//        } else {
+//            reminderService.removeReminder()
+//        }
        // updateIntervalLabel()
         self.tableView.reloadData()
     }
-    @IBOutlet weak var HeaderLabel: UILabel!
-    @IBOutlet weak var RemindSwitch: UISwitch!
     
     
     
     var feedData : [FeedingSession] = []
     var sections : [DaySection] = []
     
-    @IBOutlet weak var nextFeedingCountdownLabel: UILabel!
-    
     //UI
-    @IBOutlet weak var NextFeedingTimeSwitchLabel: UILabel!
     var slider: Slider!
     var maskView: UIView!
     var hint: UILabel?
     var titSelector : UITitSelector!
+    var bell : UIButton!
     
     
     let cellReuseIdentifier = "cellId"
@@ -61,8 +58,6 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
         reloadDataSource()
         
         initComponents()
-        
-        
     }
     
     fileprivate func reloadDataSource(){
@@ -94,50 +89,49 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
         let section =  self.sections[section]
+        
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.dateFormat = "EEEE, d MMM"
       
         return dateFormatter.string(from: section.day)
     }
     
     func updateIntervalLabel1 () {
-        reminderService.getNextReminderTime() { intervalText in
-            
-            DispatchQueue.main.async {
-                
-                self.RemindSwitch.isOn = intervalText != nil
-                
-                if let interval = intervalText {
-                    self.nextFeedingCountdownLabel.text = "\(interval)"
-                }
-                else
-                {
-                    if(self.feedData.count > 0){
-                        
-                        let ft = FeedingTime(feedingSession: self.feedData.first!)
-                        
-                        if ft.nextFeedingInterval() > 0 {
-                            self.nextFeedingCountdownLabel.text = ft.redableNextFeedingInterval()
-                        } else {
-                            self.nextFeedingCountdownLabel.text = FeedingTime.defaultRedableNextFeedingTime()
-                        }
-                    }else{
-                         self.nextFeedingCountdownLabel.text = FeedingTime.defaultRedableNextFeedingTime()
-                    }
-                  
-                }
-            }
-        }
+//        reminderService.getNextReminderTime() { intervalText in
+//
+//            DispatchQueue.main.async {
+        
+//                self.RemindSwitch.isOn = intervalText != nil
+//
+//                if let interval = intervalText {
+//                    self.nextFeedingCountdownLabel.text = "\(interval)"
+//                }
+//                else
+//                {
+//                    if(self.feedData.count > 0){
+//
+//                        let ft = FeedingTime(feedingSession: self.feedData.first!)
+//
+//                        if ft.nextFeedingInterval() > 0 {
+//                            self.nextFeedingCountdownLabel.text = ft.redableNextFeedingInterval()
+//                        } else {
+//                            self.nextFeedingCountdownLabel.text = FeedingTime.defaultRedableNextFeedingTime()
+//                        }
+//                    }else{
+//                         self.nextFeedingCountdownLabel.text = FeedingTime.defaultRedableNextFeedingTime()
+//                    }
+//
+//                }
+//            }
+        //}
         
     }
     
     func initComponents(){
         tableView.delegate = self
         tableView.dataSource = self
- 
         // reminder
        // updateIntervalLabel()
-        
         
         //drawer
         maskView = MaskView(
@@ -150,9 +144,22 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
             frame: CGRect(x: (view.frame.width / 2) - (tableViewConsts.buttonSize / 2), y: view.frame.height - tableViewConsts.bottomMargin, width: tableViewConsts.buttonSize, height: tableViewConsts.buttonSize))
         
         titSelector.delegate = self
-        
+        titSelector.layer.zPosition = 1001
         
         view.addSubview(titSelector)
+        
+        titSelector.layer.zPosition = 1001
+//        //bell button
+//        bell = UIButton(
+//            frame: CGRect(x: (view.frame.width / 2) - (tableViewConsts.buttonSize / 2) - 50 , y: view.frame.height - tableViewConsts.bottomMargin, width: tableViewConsts.buttonSize, height: tableViewConsts.buttonSize))
+//        
+//        bell.setImage(#imageLiteral(resourceName: "bell"), for: .normal)
+//        bell.setImage(#imageLiteral(resourceName: "bell"), for: .selected)
+//        bell.tintColor = #colorLiteral(red: 0.8349999785, green: 0.5609999895, blue: 0.474999994, alpha: 1)
+//        bell.contentMode = .scaleToFill
+        
+        
+//        view.addSubview(bell)
         
         
         //slider
@@ -161,6 +168,7 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
         
         view.addSubview(slider)
         
+        slider.layer.zPosition = 2
         slider.isHidden = true;
         
         
@@ -183,8 +191,9 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
         }
         
         button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
-        
-        
+        button.layer.zPosition = 1001
+        self.view.bringSubviewToFront(button)
+        button.layer.zPosition = 1111
         showHint(withText: "Select Side".localized)
         maskView.fadeIn()
         
@@ -227,7 +236,10 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
     private func showTimeButton(){
         slider.isHidden = false
         titSelector.isHidden = true
+        
+        self.tableView.isScrollEnabled = true
         showHint(withText: "Set Duration".localized)
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -237,7 +249,8 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
-        if indexPath.row == 0 && indexPath.section == 0 {
+        
+	        if indexPath.row == 0 && indexPath.section == 0 {
             return 65.0;
         }
         return 50.0;//Choose your custom row height
@@ -286,6 +299,26 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
     
     
     
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if section == 0 {
+            return UIView()
+        }
+        
+        let view = UIView(frame: CGRect(x: 20, y: section == 0 ? 30 : 10, width: 200, height: 30))
+        view.isUserInteractionEnabled = false
+        let label = UILabel(frame: CGRect(x: 0, y:section == 0 ? 28 : 8, width: 200, height: 30))
+        label.transform =  CGAffineTransform(translationX: 20.0, y: 0)
+        label.textAlignment = .left
+        label.tag = section
+        let text =  NSAttributedString(string: self.tableView(tableView, titleForHeaderInSection: section) ?? "", attributes: [.font:  UIFont(name: "Heebo-Light", size: 12.0)!, .foregroundColor: #colorLiteral(red: 0.3695555983, green: 0.3659325042, blue: 0.3659325042, alpha: 1)])
+        label.isUserInteractionEnabled = false
+        label.attributedText = text
+        view.addSubview(label)
+        return view
+        
+    }
+
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
       
         if editingStyle == .delete {
@@ -299,6 +332,11 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
            
         }
     }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return  slider?.isHidden ?? true
+    }
+    
+   
     
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
@@ -314,7 +352,9 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
         
         slider.frame =
             CGRect(x: 25, y: view.frame.height - tableViewConsts.bottomMargin - 50 + dy + 20, width: view.frame.width - 50, height: tableViewConsts.buttonSize)
-        
+//
+//        bell.frame =    CGRect(x: (view.frame.width) - (tableViewConsts.buttonSize + 8), y: 20 + dy, width: tableViewConsts.buttonSize, height: tableViewConsts.buttonSize)
+//
         if (!scrollView.isDecelerating){
             maskView.fadeOut()
             titSelector.hideButtons(0.2)
@@ -323,17 +363,24 @@ class TableViewController: UITableViewController, CircleMenuDelegate {
             hint?.isHidden = true
         }
         
+        
     }
-    
+    public override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle
+    {
+        if slider.isHidden {
+            return UITableViewCell.EditingStyle.delete
+        } else {
+            return UITableViewCell.EditingStyle.none
+        }
+    }
     
     private func configureAddingFeedingSession(side : Side){
         slider.didEndTracking = { (slider) -> () in
             slider.isHidden = true;
-            
+          
             self.maskView.fadeOut()
             self.titSelector.isHidden = false;
-            
-            self.titSelector.isUserInteractionEnabled = true
+            self.tableView.isScrollEnabled = true
             
             let newFeedingSession = FeedingSession.Create(side: side, duration: Int(slider.fraction * CGFloat(consts.maxFeedingTime)), endTime: Date())
             
